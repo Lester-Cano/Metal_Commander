e34905df79fbd3c9eff1ebdcb7647b7b46728ec0
Julian Rosero <julian.rosero@outlook.es>
2022-02-21 19:53:48 -0500

    Testing PathFinding

---
+++ Assets/Scripts/PathFinding/PathMovement.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+namespace UnitSystem
+{
+    public class PathMovement : MonoBehaviour
+    {
+        [SerializeField] private Pathfinding2D _pathfinding2D;
+        [SerializeField] private GameObject seeker;
+        [SerializeField] private GameObject taget;
+        private Vector3 worldPosition;
+
+        private void Start()
+        {
+            worldPosition = new Vector3(0,0,0);
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            
+            _pathfinding2D.FindPath(seeker.transform.position, worldPosition);
+        }
+    }
+}
